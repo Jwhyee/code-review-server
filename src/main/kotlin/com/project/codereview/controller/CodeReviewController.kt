ad4d59eb5990e95d6ed2bacd68b9529fd345def0
Jwhyee <kawnsdud@naver.com>
2025-10-23 20:32:51 +0900

    add : create google.genai client

--- src/main/kotlin/com/project/codereview/controller/CodeReviewController.kt
+++ src/main/kotlin/com/project/codereview/controller/CodeReviewController.kt
@@ -1,21 +1,16 @@
 package com.project.codereview.controller
 
-import com.google.genai.Client
 import com.google.genai.types.*
-import com.project.codereview.service.CodeReviewService
+import com.project.codereview.util.GenClient
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/webhook")
-class WebhookController(
-    private val codeReviewService: CodeReviewService,
-) {
-
-
+@RequestMapping("/review")
+class CodeReviewController {
     @PostMapping
     fun net() {
-        codeReviewService.review()
+
     }
 }
--- src/main/kotlin/com/project/codereview/util/GenClient.kt
+++ src/main/kotlin/com/project/codereview/util/GenClient.kt
@@ -1,4 +1,35 @@
 package com.project.codereview.util
 
+import com.google.genai.Client
+import com.google.genai.types.Content
+import com.google.genai.types.GenerateContentConfig
+import com.google.genai.types.Part
+import com.google.genai.types.ThinkingConfig
+
 object GenClient {
+    private val client: Client = Client.builder().apiKey(API_KEY).build()
+
+    private val instruction = Content.builder()
+        .role("system")
+        .parts(
+            listOf(
+                Part.builder()
+                    .text(SYSTEM_PROMPT)
+                    .build()
+            )
+        )
+        .build()
+
+    private val think = ThinkingConfig.builder()
+        .thinkingBudget(0)
+        .build()
+
+    fun chat(prompt: String) = client.models.generateContent(
+        MODEL,
+        prompt,
+        GenerateContentConfig.builder()
+            .systemInstruction(instruction)
+            .thinkingConfig(think)
+            .build()
+    ).text() ?: throw GenerateException("Generating fail")
 }
