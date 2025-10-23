32e36b2c3b97abeeb8612c8f76b6335741fc9f7e
Jwhyee <kawnsdud@naver.com>
2025-10-23 19:38:45 +0900

    init

---
+++ src/main/kotlin/com/project/codereview/CodeReviewApplication.kt
@@ -0,0 +1,11 @@
+package com.project.codereview
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CodeReviewApplication
+
+fun main(args: Array<String>) {
+    runApplication<CodeReviewApplication>(*args)
+}
