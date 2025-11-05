c8f41794125fafc227df4a0882b1f2fb97012f41
Jwhyee <kawnsdud@naver.com>
2025-11-05 19:04:58 +0900

    refactor : seperate gemini, github error catching

--- src/main/kotlin/com/project/codereview/core/service/ReviewWorker.kt
+++ src/main/kotlin/com/project/codereview/core/service/ReviewWorker.kt
@@ -5,6 +5,7 @@ import com.project.codereview.client.github.GithubReviewClient
 import com.project.codereview.client.google.GoogleGeminiClient
 import com.project.codereview.core.dto.GithubPayload
 import com.project.codereview.core.dto.GithubReviewDto
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
@@ -13,16 +14,38 @@ class ReviewWorker(
     private val githubReviewClient: GithubReviewClient,
     private val failedTaskManager: FailedTaskManager
 ) {
+    private val logger = LoggerFactory.getLogger(ReviewWorker::class.java)
+
     suspend fun process(payload: GithubPayload, task: DiffTaskPreparer.ReviewTask) {
         val prompt = "```diff\n${task.part.content}\n```"
         val filePath = task.part.filePath
 
+        val reviewResult = runCatching {
+            googleGeminiClient.chat(filePath, prompt)
+        }.onFailure { e ->
+            logger.warn("[Gemini error] - $payload, $task")
+            logger.warn("[Gemini error] - {}", e.message)
+            handleGeminiError(e.message ?: "", prompt, payload, task)
+        }
+
+        val review = reviewResult.getOrNull() ?: return
+
         runCatching {
-            val review = googleGeminiClient.chat(filePath, prompt)
-            if (review != null) {
-                githubReviewClient.addReviewComment(GithubReviewDto(task.payload, task.part, payload.installation.id, review))
-            }
-        }.onFailure {
+            githubReviewClient.addReviewComment(GithubReviewDto(task.payload, task.part, payload.installation.id, review))
+        }.onFailure { e ->
+            logger.warn("[Github error] - $payload, $task")
+            logger.warn("[Github error] - cause {}", e.message)
+        }
+    }
+
+    private fun handleGeminiError(
+        e: String,
+        prompt: String,
+        payload: GithubPayload,
+        task: DiffTaskPreparer.ReviewTask
+    ) {
+        val isTooManyRequestError = e.contains("429 Too Many Requests") || e.contains("429")
+        if (isTooManyRequestError) {
             failedTaskManager.add(FailedTaskManager.OriginalTask(payload, task.part), prompt)
         }
     }
