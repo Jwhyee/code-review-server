552da65474707745fc9675ae293a7aecab9a49db
Jwhyee <kawnsdud@naver.com>
2025-11-07 21:30:16 +0900

    refactor : 사용하지 않는 DTO 제거 및 ReviewContext 적용

--- src/main/kotlin/com/project/codereview/client/github/GithubReviewClient.kt
+++ src/main/kotlin/com/project/codereview/client/github/GithubReviewClient.kt
@@ -1,8 +1,6 @@
 package com.project.codereview.client.github
 
-import com.project.codereview.core.dto.GithubReviewDto
-import com.project.codereview.core.dto.PullRequestPayload
-import org.springframework.beans.factory.annotation.Value
+import com.project.codereview.client.github.dto.ReviewContext
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 
@@ -10,37 +8,24 @@ import org.springframework.web.reactive.function.client.WebClient
 class GithubReviewClient(
     private val tokenProvider: GithubAppTokenProvider
 ) {
-    data class ReviewCommentRequest(
-        val body: String,
-        val path: String,
-        val commit_id: String,
-        val line: Int? = null,
-        val side: String? = null,
-        val start_line: Int? = null,
-        val start_side: String? = null
-    )
-
     val client = WebClient.builder()
         .baseUrl("https://api.github.com")
         .defaultHeader("Accept", "application/vnd.github+json")
         .build()
 
     suspend fun addReviewSummaryComment(
-        payload: PullRequestPayload,
-        installationId: String,
-        commitId: String,
-        comment: String
+        ctx: ReviewContext
     ) {
-        val uri = payload.run {
+        val uri = ctx.run {
             "/repos/$owner/$repo/pulls/$prNumber/reviews"
         }
         val payload = mutableMapOf(
             "event" to "COMMENT",
-            "commit_id" to commitId,
-            "body" to comment
+            "commit_id" to ctx.commitId,
+            "body" to ctx.body
         )
 
-        val token = tokenProvider.getInstallationToken(installationId)
+        val token = tokenProvider.getInstallationToken(ctx.installationId)
 
         client.post()
             .uri(uri)
@@ -52,20 +37,18 @@ class GithubReviewClient(
     }
 
     suspend fun addReviewComment(
-        dto: GithubReviewDto
+        ctx: ReviewContext
     ) {
-        val uri = dto.payload.run {
+        val uri = ctx.run {
             "/repos/$owner/$repo/pulls/$prNumber/comments"
         }
-
-        println("dto.toReviewCommentRequest() = ${dto.toReviewCommentRequest()}")
 
         client.post()
             .uri(uri)
             .headers {
-                it.setBearerAuth(tokenProvider.getInstallationToken(dto.installationId))
+                it.setBearerAuth(tokenProvider.getInstallationToken(ctx.installationId))
             }
-            .bodyValue(dto.toReviewCommentRequest())
+            .bodyValue(ctx.type.toMap(ctx.body, ctx.installationId))
             .retrieve()
             .bodyToMono(String::class.java)
             .block()
